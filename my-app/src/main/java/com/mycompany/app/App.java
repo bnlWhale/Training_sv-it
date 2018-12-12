627052e254926dc38c0327b92416f295b8a91cf8
Roland <ducounau@gmail.com>
2018-12-12 02:42:14 -0800

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,17 @@ import algo.bookoop.BookBuilder;
    key point:  0).Publication served as abstract class due to field isbn and publisher were immutable.
                1). programmer to interface(IContent)
  *             2). design pattern: decorator, builder, composition
+ 
+     ran app and output:
+      this book's content is:
+      html_table_of_content
+      Chapter 0
+       section 0
+         paragraph 0
+         paragraph 1
+       section 1
+         paragraph 2
+         paragraph 3
  */
 public class App 
 {
