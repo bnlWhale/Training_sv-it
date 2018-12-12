74a1a263591dfd6dc3b4a3088e4b89ff301d6096
Roland <ducounau@gmail.com>
2018-12-12 02:42:46 -0800

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import algo.bookoop.BookBuilder;
                1). programmer to interface(IContent)
  *             2). design pattern: decorator, builder, composition
  
-     ran app and output:
+     ran app and output =>
       this book's content is:
       html_table_of_content
       Chapter 0
